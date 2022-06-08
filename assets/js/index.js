9a23e7f32a03d70cd2f1c1a2275def79819be1aa
guitarkeegan <keegananglim@gmail.com>
2022-06-08 00:04:35 -0700

    display recent searches on screen. bug with printRecentSearches function

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,20 +8,10 @@ const headerEl = $("header");
 const mainEl = $("main");
 const searchInputEl = $("#city-searched");
 const searchFormEl = $("#search-form");
+const recentSearchesEl = $("#recent-searches");
 // event listenters
 $("#search-button").on("click", getCity);
 
-// will give the lat and long, name, state, country. You can also omitt the country
-// {
-//   "name": "Torrance",
-//   "local_names": {
-//       "en": "Torrance"
-//   },
-//   "lat": 33.8358492,
-//   "lon": -118.3406288,
-//   "country": "US",
-//   "state": "California"
-// }
 function getCity() {
   cityName = searchInputEl.val();
   const limit = 1;
@@ -32,6 +22,8 @@ function getCity() {
       const lon = data[0].lon;
       getCurrentWeather(lat, lon);
     });
+  handleRecentSearches(cityName);
+  printRecentSearches();
 }
 
 function printSearchedCity(currentData){
@@ -43,13 +35,7 @@ function printSearchedCity(currentData){
   const icon = getIcon(currentData.weather[0].icon);
   const description = currentData.weather[0].description;
 }
-// data.current.dt for current time in timecode
-// data.current.temp for temperature
-// data.current.wind_speed for current wind speed
-// data.current.humidity for humidity
-// data.current.uvi for uv index
-// data.current.weather[0].icon for special icon
-// data.current.weather[0].description for brief description
+
 function getCurrentWeather(lat, lon){
   fetch(`https://api.openweathermap.org/data/2.5/onecall?exclude=minutely&units=imperial&lat=${lat}&lon=${lon}&appid=${dontGetExcitedItsFree}`)
   .then(response => response.json())
@@ -68,15 +54,15 @@ function printFiveDayForcast(daily){
   const description = daily.weather[0].description;
 }
 
+function printRecentSearches(){
+  // TODO: removed anything already displayed on the page
+  recentSearches.forEach((city)=>{
+    const cityEl = $("<button>").text(city);
+    cityEl.addClass("recent-search-item btn");
+    recentSearchesEl.append(cityEl);
+  })
+}
 
-// data.daily[i].dt for timecode
-// data.daily[i].weather[0].icon
-// data.daily[i].temp.min 
-// data.daily[i].temp.max
-// data.daily[i].temp.day
-// data.daily[i].wind_speed
-// data.daily[i].humidity
-// data.daily[i].weather[0].description
 function getFiveDayForcast(lat, lon){
   fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&&appid=${dontGetExcitedItsFree}`)
   .then(response => response.json())
@@ -86,11 +72,7 @@ function getFiveDayForcast(lat, lon){
     }
   });
 }
-// TODO: get 5 day forcast from API
-// TODO: single city focus gives: cityname, the date, icon for conditions, favorable, moderate, severe
-// TODO: 5 day forcast gives, date, icon for conditions, temp, wind speed, humidity
-// TODO: search history in local storage. 8 in history in the example.
-// TODO: foundation might be a good bootsrap alternative
+
 
 function getIcon(iconCode){
   fetch(`http://openweathermap.org/img/wn/${iconCode}@2x.png`)
@@ -111,12 +93,4 @@ function handleRecentSearches(cityName){
   }
 }
 
-
-
-
-
-// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-// call to get lat and lon
-
-
-
+// TODO: search history in local storage. 8 in history in the example.
